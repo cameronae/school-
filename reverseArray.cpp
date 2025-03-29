7492ab1eb7cf343a0914d3d491dd4448b05c93a9
cameronae <cameronjreiff@yahoo.com>
2025-03-29 02:32:03 -0400

    finish the night

---
+++ reverseArray.cpp
@@ -0,0 +1,30 @@
+//One way to reverse an array is to swap the first and last and repeat
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int userInts[5];
+    int n = sizeof(userInts) / sizeof(userInts[0]);
+    int temp;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> userInts[i];
+    }
+    
+    for (int i = 0; i < (5/2); i++)
+    {
+        int temp = userInts[i];
+        userInts[i] = userInts[n - 1 - i];
+        userInts[n - 1 - i] = temp;
+        
+    }
+    
+    for (int i = 0; i < 5; i++)
+        cout << userInts[i] << " ";
+
+    cout << endl;
+    
+}
