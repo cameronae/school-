27cbf9f3099891c24837720058bd3a97068b7814
cameronae <cameronjreiff@yahoo.com>
2025-03-30 22:59:51 -0400

    binary search

--- algorithms/binarySearch.cpp
+++ algorithms/binarySearch.cpp
@@ -3,12 +3,28 @@
 
 int main()
 {
-    int size = 10;
-    int array[] = { 1, 2, 3, 4, 5, 6 , 7, 8, 9, 10 };
-    int target = 2;
+    int right = 9;
+    int left = 0;
+    int mid;
+    bool found = false;
+    int array[10] = {1, 2, 3, 4, 5, 6 , 7, 8, 9, 10};
+    int target = 5;
 
-    for (int i = 0; i < size; i++){
-        
+    while (left <= right && !found)
+    {
+        mid = (right + left) / 2;
+        if (array[mid] == target){
+            found = true;
+            std:: cout << "found target at " << mid;
+        }
+        else if(array[mid] < target){
+            left = mid - 1;
+            found = false;
+        }
+        else{
+            right = mid + 1;
+            found = false;
+        }
     }
-
+    return 0;
 }
