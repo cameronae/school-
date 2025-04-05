642d3613798e0539dc4854257475691031daccf0
cameronae <cameronjreiff@yahoo.com>
2025-04-04 20:46:31 -0400

    encrptLab

--- labs/encryptDecryptLab.cpp
+++ labs/encryptDecryptLab.cpp
@@ -11,25 +11,25 @@
      cin >> str;
      //------------
      // this statement creates a pointer to the string you enter so that you can treat the string as a character array 
+     //IMPORTANT
      const char* charArray = str.c_str();
      
   // the length() function in the string library returns a long unsigned int 
   //by delcaring here it avoids 2 warnings from the compiler
- long unsigned int i;
+    long unsigned int i;
  
- 
-     // Print the characters using an character array index 
-     for ( i = 0; i < str.length(); ++i) {
-         cout <<"letters in char array " <<charArray[i] << std::endl;
-     }
  //-----------------
      // write a for loop to add 3 to each ascii value in the string
  //your code here 
+     for (i = 0; i < str.length(); i++){
+        str[i] += 3;
+     }
      cout << "\nEncrypted string with offset of 3: " << str << endl;
- 
      // Write a for loop to subtract 3 to each ascii value in the string
  //your code here 
- 
+     for (i = 0; i <str.length(); i++){
+        str[i] -= 3;
+     }
      cout << "\nDecrypted string with offset of 3 : " << str << endl;
  
  
